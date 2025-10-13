d1512e0b42beb74f672c59a6a5df70846022186b
duvindu-embuldeniya <duvindu.personal@gmail.com>
2025-10-14 00:05:01 +0530

    mixins -> get,post

--- backend-django/Phase-1/urls.py
+++ backend-django/Phase-1/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     # path('customer/<int:pk>/', get_customer, name = 'customer'),
 
     #Class Based Views.................................................................
-    path('customer/', customer.as_view(), name = 'customer'),
-    path('get_customer/<int:pk>/', get_customer.as_view(), name = 'customer'),
+    # path('customer/', customer.as_view(), name = 'customer'),
+    # path('get_customer/<int:pk>/', get_customer.as_view(), name = 'customer'),
+
+    # Mixins.........................................................................
+    path('customer/', customer.as_view(), name = 'employee'),
 
 ]
--- backend-django/Phase-1/views.py
+++ backend-django/Phase-1/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework import mixins,generics,viewsets
 
 
 
@@ -53,43 +54,60 @@ from rest_framework.views import APIView
 
 
 
+
 #Class Based Views.................................................................
-class customer(APIView):
-    def get(self, request):
-        data = Customer.objects.all()
-        serializer = CustomerSerializer(data, many=True)
-        return Response(serializer.data)
+# class customer(APIView):
+#     def get(self, request):
+#         data = Customer.objects.all()
+#         serializer = CustomerSerializer(data, many=True)
+#         return Response(serializer.data)
     
-    def post(self,request):
-        serializer = CustomerSerializer(data = request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+#     def post(self,request):
+#         serializer = CustomerSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-class get_customer(APIView):
-    def get_object(self, pk):
-        try:
-            return Customer.objects.get(pk=pk)
-        except Customer.DoesNotExist:
-            raise Http404
+# class get_customer(APIView):
+#     def get_object(self, pk):
+#         try:
+#             return Customer.objects.get(pk=pk)
+#         except Customer.DoesNotExist:
+#             raise Http404
         
-    def get(self, request, pk):
-        data = self.get_object(pk)
-        serializer = CustomerSerializer(data, many=False)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+#     def get(self, request, pk):
+#         data = self.get_object(pk)
+#         serializer = CustomerSerializer(data, many=False)
+#         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def put(self, request, pk):
-        data = self.get_object(pk)
-        serializer = CustomerSerializer(data, request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+#     def put(self, request, pk):
+#         data = self.get_object(pk)
+#         serializer = CustomerSerializer(data, request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-    def delete(self, request, pk):
-        data = self.get_object(pk)
-        data.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+#     def delete(self, request, pk):
+#         data = self.get_object(pk)
+#         data.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+# Mixins.........................................................................
+class customer(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+
+    queryset = Customer.objects.all()
+    serializer_class = CustomerSerializer
+
+    def get(self, request):
+        return self.list(request)
+    
+    def post(self, request):
+        return self.create(request)
