68e92613aef8f8425eff0dc0afd99f440092a60c
duvindu-embuldeniya <duvindu.personal@gmail.com>
2025-10-15 12:47:33 +0530

    refactor protected view with response interceptor

--- frontend-react/src/axisoinstance.js
+++ frontend-react/src/axisoinstance.js
@@ -36,6 +36,42 @@ axiosInstance.interceptors.request.use(
 
 
 
+// Response Interceptor
+axiosInstance.interceptors.response.use(
+    function(response){
+        return response
+    },
+
+
+    //Handle failed responses
+    async function(error){ //error like 401
+
+        const response = error.config //store the failed request (wht we sent and failed)
+        // if(error.response.status === 401 && !response.retry){
+        //     response.retry = true
+        if(error.response.status === 401){
+
+            const refereshToken = localStorage.getItem('refreshToken')
+
+            try{
+                const response = await axios.post('http://localhost:8000/api/p2/token/refresh/', {refresh: refereshToken})
+                // console.log('New Access Token :- ',response.data.access)
+                // try to get new access token using existing refrsh token
+
+                localStorage.setItem('accessToken', response.data.access)
+                response.headers['Authorization'] = `Bearer ${response.data.access}`
+                return axiosInstance(response)
+                
+            }catch(error){ //if refresh token has expired...........
+                localStorage.removeItem('accessToken')
+                localStorage.removeItem('refreshToken')
+                
+                window.location.href = '/login'
+            }
+        }
+        // return Promise.reject(error)
+    }
+)
 
 
 
